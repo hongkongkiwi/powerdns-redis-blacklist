a42e798906becec23192a72809f68352ae49640d
Andy <andy@savage.hk>
2016-01-27 02:40:03 +0900

    Update serve.js
    Using HTTP server for now instead of zeromq

--- serve.js
+++ serve.js
@@ -1,10 +1,55 @@
-// worker.js
-var zmq = require('zmq'),
-    sock = zmq.socket('pull');
+var express = require('express');
+var app = express();
+var dns = require('dns');
+var util = require('util');
+var _ = require('underscore');
+var redis = require("redis"),
+    client = redis.createClient();
 
-sock.connect('ipc:///tmp/pdns.sock');
-console.log('Worker connected to ipc');
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
-sock.on('message', function(msg){
-  console.log('work: %s', msg.toString());
+var upstreamServer = { address: '8.8.8.8', port: 53, type: 'udp' };
+var timeout = 1000;
+
+app.get('/dns/:method/:domain/:type', function (req, res) {
+	console.log('request',req.headers);
+	remoteIp = req.headers['x-remotebackend-real-remote'];
+
+	// Get the category of the domain name
+	client.smembers(req.params.domain + ':category', function (err, domainCategories) {
+		replies.forEach(function (reply, i) {
+			client.ismember('ip:' + remoteIp + ':category', category, function (err, isMember) {
+				console.log('Found category!');
+			});
+		}
+	});
+
+	// Get the allowed categories of the ip
+
+	// Check if they match
+
+	// No match set 404
+	res.sendStatus(404);
+
+	/*if (req.params.method === 'lookup') {
+		console.log(req.params);
+		if (req.params.type === 'ANY') return;
+		dns.resolve(req.params.domain, req.params.type, function(err, address) {
+			console.log(address);
+			if (req.params.type === 'SOA') {
+				var dnsReply = {qtype:req.params.type,qname:req.params.domain,content: address.nsname + ". " + address.hostmaster + ". " + address.serial + " " + address.refresh + " " + address.retry + " " + address.expire + " " + address.minttl,ttl:address.minttl};
+				console.log(dnsReply);
+				res.json({result: [dnsReply]});
+			} else {
+			}
+		});
+	} else {
+
+	}*/
+});
+
+app.listen(5353, function () {
+  console.log('Example app listening on port 5353');
 });
